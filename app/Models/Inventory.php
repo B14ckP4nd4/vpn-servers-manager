5c79a8b71702de83d0fad4ca6f4813e6b0aea578
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2021-12-16 22:56:08 +0000

    add relation and properties to Inventory Model

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Inventory extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    protected $fillable = ['name','is_active'];
+
+    /**
+     * determine items relation
+     * @return HasMany
+     */
+    public function items(){
+        return $this->hasMany(InventoryItem::class);
+    }
 }
