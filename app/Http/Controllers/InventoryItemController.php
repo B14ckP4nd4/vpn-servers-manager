a2a780124ff288b3ef5dd1b9570af6809b79de7c
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2021-12-17 22:56:38 +0000

    create controller

--- app/Http/Controllers/InventoryItemController.php
+++ app/Http/Controllers/InventoryItemController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\InventoryItem;
 use Illuminate\Http\Request;
 
 class InventoryItemController extends Controller
 {
-    //
+    protected $viewPath = 'dashboard.inventory-items.';
+
+    public function index()
+    {
+        return view($this->viewPath . 'index');
+    }
 }
