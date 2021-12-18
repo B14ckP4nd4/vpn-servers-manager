83aed6a9a255bdd7b935954e53cbe18e6b4d5f6f
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2021-12-18 21:04:37 +0000

    add alpine for emit create inventory event to directly

--- resources/views/dashboard/inventory-items/index.blade.php
+++ resources/views/dashboard/inventory-items/index.blade.php
@@ -17,9 +17,9 @@
                         <!-- Page title actions -->
                         <div class="col-auto ms-auto d-print-none">
                             <div class="btn-list">
-                                <a href="#"
+                                <a x-data @click="window.Livewire.emitTo('inventory-items-form-modal','createItem')" href="#"
                                    class="btn btn-primary d-none d-sm-inline-block" data-bs-toggle="modal"
-                                   data-bs-target="#modal_inventory_form">
+                                   data-bs-target="#inventory-items-form">
                                     <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
                                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
                                          viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
