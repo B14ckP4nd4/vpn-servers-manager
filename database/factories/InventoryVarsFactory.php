d735eb6179e77c5160bea0151ef71848978816d4
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2021-12-26 16:53:04 +0000

    create inventory var factory

---
+++ database/factories/InventoryVarsFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace Database\Factories;
+
+
+use App\Models\Inventory;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class InventoryVarsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => "var_" . $this->faker->regexify('[A-Za-z0-9]{10}'),
+            'val' => $this->faker->regexify('[A-Za-z0-9]{20}'),
+            'inventory_id' => $this->getRandomInventoryID(),
+        ];
+    }
+
+    protected function getRandomInventoryID(){
+        return Inventory::inRandomOrder()->limit(1)->first()->id;
+    }
+
+}
