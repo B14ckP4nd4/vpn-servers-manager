0b49a9ca9f15ff5a18312f68a1b09017055e1f22
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2021-12-26 15:49:59 +0000

    add inventory var properties and releation

--- app/Models/InventoryVar.php
+++ app/Models/InventoryVar.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class InventoryVar extends Model
 {
     use HasFactory;
+    protected $table = 'inventory_vars';
+    protected $fillable = ['name','inventory_id','val'];
+
+    /**
+     * determine inventory relation
+     * @return BelongsTo
+     */
+    public function inventory()
+    {
+        return $this->belongsTo(Inventory::class,'inventory_id','id');
+    }
 }
