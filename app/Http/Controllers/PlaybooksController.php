6cde88e6e9417a58fd3b95da6afc6c894c7dc0d3
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2021-12-27 08:32:11 +0000

    add index method

--- app/Http/Controllers/PlaybooksController.php
+++ app/Http/Controllers/PlaybooksController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class PlaybooksController extends Controller
 {
-    //
+    protected $viewPath = 'dashboard.playbooks.';
+
+    public function index()
+    {
+        return view($this->viewPath . 'index');
+    }
 }
