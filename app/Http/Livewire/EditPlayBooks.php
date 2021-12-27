b6690369123be728cd9cf93fdffe176634808655
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2021-12-27 20:28:36 +0000

    edit playbook livewire component

---
+++ app/Http/Livewire/EditPlayBooks.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Events\PlayBookAdded;
+use Illuminate\Validation\Rule;
+use Livewire\Component;
+
+class EditPlayBooks extends Component
+{
+    public $selected, $name, $url, $path;
+    public $updated = false;
+
+    protected $listeners = [
+        'pleaseRefresh' => '$refresh',
+        'selectForEdit' => 'select'
+    ];
+
+    public function render()
+    {
+        return view('livewire.edit-play-books');
+    }
+
+    public function select($id)
+    {
+        $this->resetForm();
+        $playBook = \App\Models\PlayBooks::findOrFail($id);
+        $this->name = $playBook->name;
+        $this->selected = $playBook->id;
+        $this->url = $playBook->url;
+        $this->path = $playBook->path;
+    }
+
+    public function resetForm()
+    {
+        $this->selected = $this->name = $this->url = $this->path = null;
+        $this->updated = false;
+        $this->reset('selected','name','url','path');
+        $this->resetErrorBag();
+        $this->resetValidation();
+        $this->emitSelf('pleaseRefresh');
+        $this->emitTo('playbooks','pleaseRefresh');
+    }
+
+    public function update()
+    {
+        $this->validate([
+            'name' => ['required','string','min:4','max:255',Rule::unique('playbooks')->ignore($this->selected)],
+            'url' => ['required','string','min:4','max:255',Rule::unique('playbooks')->ignore($this->selected)],
+        ]);
+
+        $playBook = \App\Models\PlayBooks::findOrFail($this->selected)->update([
+            'name' => $this->name,
+            'url' => $this->url,
+        ]);
+
+        $this->resetForm();
+        PlayBookAdded::dispatch($playBook);
+
+        $this->updated = true;
+    }
+
+
+}
---
+++ resources/views/livewire/edit-play-books.blade.php
@@ -0,0 +1,51 @@
+<div>
+    <div class="modal-header">
+        <h5 class="modal-title">Edit PlayBook</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" wire:click="resetForm()"
+                aria-label="Close"></button>
+    </div>
+    <div class="modal-body">
+        <div class="mb-3">
+            <label class="form-label">name</label>
+            <input type="text" wire:model="name" class="form-control" name="example-text-input"
+                   placeholder="Enter PlayBook Name">
+            @error('name') <span class="error">{{ $message }}</span> @enderror
+        </div>
+        <div class="mb-3">
+            <label class="form-label">URL</label>
+            <input type="text" wire:model="url" class="form-control" name="example-text-input"
+                   placeholder="Enter PlayBook URL">
+            @error('val') <span class="error">{{ $message }}</span> @enderror
+        </div>
+
+    </div>
+    <div class="modal-footer">
+        <a href="#" data-bs-dismiss="modal" wire:click="resetForm()" class="btn btn-link link-secondary">
+            Cancel
+        </a>
+        @if(!$updated && $selected)
+            <a href="#" wire:click="update()" class="btn btn-primary ms-auto">
+                <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                     stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <line x1="12" y1="5" x2="12" y2="19"></line>
+                    <line x1="5" y1="12" x2="19" y2="12"></line>
+                </svg>
+                Update
+            </a>
+        @endif
+
+        @if($updated)
+        <a href="#" wire:click="resetForm()" class="btn btn-success ms-auto" data-bs-dismiss="modal">
+            <!-- Download SVG icon from http://tabler-icons.io/i/check -->
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                 stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M5 12l5 5l10 -10"/>
+            </svg>
+            Done
+        </a>
+        @endif
+    </div>
+</div>
