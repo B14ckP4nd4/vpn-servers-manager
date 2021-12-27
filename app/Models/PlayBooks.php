65f61f784bca802b2492923c6716232fb2465371
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2021-12-27 20:19:46 +0000

    change playbook models class name

--- app/Models/PlayBooks.php
+++ app/Models/PlayBooks.php
@@ -5,7 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class playbooks extends Model
+class PlayBooks extends Model
 {
     use HasFactory;
+    protected $fillable = ['name','path','url','last_updated_at'];
+    protected $guarded = ['id'];
+    protected $table = 'playbooks';
 }
