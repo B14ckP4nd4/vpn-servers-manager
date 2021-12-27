eabb9bb1cdff297501e139ff9b068bb0e5cbc9a6
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2021-12-27 20:22:13 +0000

    create ansible config file for store path and auth information

--- config/ansible.php
+++ config/ansible.php
@@ -1,1 +1,21 @@
 <?php
+
+return [
+    /*
+      |--------------------------------------------------------------------------
+      | Ansible Configs
+      |--------------------------------------------------------------------------
+      |
+      | This Config file Contain the Ansible Configurations
+      |
+      */
+
+    'default_path' => storage_path('ansible'),
+    'key_path' => storage_path('ansible/keys'),
+    'playbook_path' => storage_path('ansible/playbooks/'),
+    'default_username' => env('ANSIBLE_USERNAME','root'),
+    'default_password' => env('ANSIBLE_PASSWORD','NkxU9gt37Ak8CWhv'),
+    'default_configured_username' => env('ANSIBLE_CONFIGURED_USERNAME','masterAnsible'),
+    'private_key' => env('ANSIBLE_PRIVATE_KEY','ssh-auth'),
+    'public_key' => env('ANSIBLE_PRIVATE_KEY','ssh-auth.pub'),
+];
