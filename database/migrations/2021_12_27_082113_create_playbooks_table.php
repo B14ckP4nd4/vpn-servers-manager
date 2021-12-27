56173fb07a66c018746d563abbb520118fb84162
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2021-12-27 08:32:20 +0000

    add columns

--- database/migrations/2021_12_27_082113_create_playbooks_table.php
+++ database/migrations/2021_12_27_082113_create_playbooks_table.php
@@ -15,6 +15,10 @@ class CreatePlaybooksTable extends Migration
     {
         Schema::create('playbooks', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->text('path')->nullable()->unique();
+            $table->text('url')->nullable()->unique();
+            $table->dateTime('last_updated_at')->nullable();
             $table->timestamps();
         });
     }
