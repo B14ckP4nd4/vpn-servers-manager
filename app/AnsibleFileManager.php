481dc60ab9436623e92e65ce8be3bdcc3eea37ee
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2021-12-29 20:09:58 +0000

    create a method for create the file names

--- app/AnsibleFileManager.php
+++ app/AnsibleFileManager.php
@@ -4,7 +4,32 @@
 namespace App;
 
 
-class ansibleFileManager
+class AnsibleFileManager
 {
+    /**
+     * determine constructor
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * remove special chars from name
+     *
+     * @param string $name
+     * @param string | boolean $extension
+     * @return string
+     */
+    public static function clearName($name, $extension = false){
+        $file_name = str_replace(' ', '-', $name);
+        $file_name = preg_replace('/\\.[^.\\s]{3,4}$/', '', $file_name);;
+        $file_name = preg_replace('/[^A-Za-z0-9\-]/', '', $file_name);
+
+        if($extension)
+            $file_name = $file_name . '.' . $extension;
+
+        return $file_name;
+    }
 
 }
