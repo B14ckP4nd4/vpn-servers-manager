8af7a371b8a03be1a476df917b1add5bb880e8ca
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2021-12-30 20:12:25 +0000

    add path of the plays logs

--- config/ansible.php
+++ config/ansible.php
@@ -14,6 +14,7 @@ return [
     'key_path' => storage_path('ansible/keys/'),
     'playbook_path' => storage_path('ansible/playbooks/'),
     'inventories_path' => storage_path('ansible/inventories/'),
+    'play_logs_path' => storage_path('ansible/plays/'),
     'default_username' => env('ANSIBLE_USERNAME','root'),
     'default_password' => env('ANSIBLE_PASSWORD','NkxU9gt37Ak8CWhv'),
     'default_configured_username' => env('ANSIBLE_CONFIGURED_USERNAME','masterAnsible'),
