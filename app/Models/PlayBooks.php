a535a26d1a525c261217b83926b8ccc31ea0a2e0
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2022-01-01 21:49:37 +0000

    add plays relation

--- app/Models/PlayBooks.php
+++ app/Models/PlayBooks.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class PlayBooks extends Model
 {
@@ -11,4 +12,15 @@ class PlayBooks extends Model
     protected $fillable = ['name','path','url','last_updated_at'];
     protected $guarded = ['id'];
     protected $table = 'playbooks';
+
+
+
+    /**
+     * determine play relation
+     * @return HasMany
+     */
+    public function plays()
+    {
+        return $this->hasMany(Play::class, 'playbook_id', 'id');
+    }
 }
