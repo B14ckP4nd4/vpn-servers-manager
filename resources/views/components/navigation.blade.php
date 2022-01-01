e8a9a2bfc3322a6fce382448db383fb6517b0450
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2022-01-01 21:48:22 +0000

    add plays route menu item

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -135,13 +135,30 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('dashboard.playbooks') }}">
                     <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
+                      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-book" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                          <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
+                          <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
+                          <line x1="3" y1="6" x2="3" y2="19" />
+                          <line x1="12" y1="6" x2="12" y2="19" />
+                          <line x1="21" y1="6" x2="21" y2="19" />
+                        </svg>
+                    </span>
+                            <span class="nav-link-title">
+                                PlayBooks
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('dashboard.plays') }}">
+                    <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
                       <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-player-play" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                           <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                           <path d="M7 4v16l13 -8z" />
                       </svg>
                     </span>
                             <span class="nav-link-title">
-                                PlayBooks
+                                Plays
                             </span>
                         </a>
                     </li>
