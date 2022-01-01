06b1196cb261409bf00f3d2730bf1c9a4c2187c5
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2022-01-01 20:06:02 +0000

    run playbook and store logs for feature cheking

--- app/Console/Commands/RunAnsiblePlayBooks.php
+++ app/Console/Commands/RunAnsiblePlayBooks.php
@@ -2,7 +2,17 @@
 
 namespace App\Console\Commands;
 
+use App\AnsibleFileManager;
+use App\Events\PlayCompleted;
+use App\Models\Play;
+use Asm\Ansible\Ansible;
 use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Symfony\Component\Process\Process;
+
+    set_time_limit(0);
 
 class RunAnsiblePlayBooks extends Command
 {
@@ -37,7 +47,38 @@ class RunAnsiblePlayBooks extends Command
      */
     public function handle()
     {
+        $ansible = new Ansible(config('ansible.default_path'));
         $logPath = config('ansible.play_logs_path');
+
+        $plays = Play::pending()->get();
+        foreach ($plays as $play) {
+            $logFileName = AnsibleFileManager::clearName($play->id . '_' . date('Y-m-d_H-i-s'), 'log');
+            $logFile = AnsibleFileManager::saveFile($logFileName, $logPath, "");
+
+            $play->update([
+                'log_file' => $logFile,
+//                'run_at' => Carbon::now(),
+//                'is_running' => 1,
+            ]);
+
+
+            $run = $ansible->playbook()
+                ->play($play->playbook()->first()->path)
+                ->inventoryFile($play->inventory()->first()->path)
+                ->hostKeyChecking(false)
+                ->verbose()
+                ->execute();
+
+
+            $logFile = AnsibleFileManager::saveFile($logFile, null, $run);
+
+//            $play->update([
+//                'completed_at' => Carbon::now(),
+//                'is_running' => 0,
+//            ]);
+
+            PlayCompleted::dispatch($play);
+        }
 
         return Command::SUCCESS;
     }
