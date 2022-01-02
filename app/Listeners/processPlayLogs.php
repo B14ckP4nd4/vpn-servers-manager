c9ee9af5b84a3e47992a94827b2cb770f23e25ca
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2022-01-02 23:34:29 +0000

    check the play logs

--- app/Listeners/processPlayLogs.php
+++ app/Listeners/processPlayLogs.php
@@ -2,6 +2,7 @@
 
 namespace App\Listeners;
 
+use App\AnsibleFileManager;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
@@ -25,6 +26,36 @@ class processPlayLogs
      */
     public function handle($event)
     {
-        dd($event->play->log_file);
+        $play = $event->play;
+        $content = AnsibleFileManager::loadFile($event->play->log_file);
+        $runsLog = AnsibleFileManager::parsePlayLog($content);
+
+        $stats = [
+            'ok' => 0,
+            'changed' => 0,
+            'unreachable' => 0,
+            'skipped' => 0,
+            'rescued' => 0,
+            'ignored' => 0,
+        ];
+
+        if($runsLog){
+            foreach ($runsLog as $item){
+                $stats['ok'] += $item['ok'];
+                $stats['changed'] += $item['changed'];
+                $stats['unreachable'] += $item['unreachable'];
+                $stats['skipped'] += $item['skipped'];
+                $stats['rescued'] += $item['rescued'];
+                $stats['ignored'] += $item['ignored'];
+            }
+        }
+
+        $play->ok = $stats['ok'];
+        $play->changed = $stats['changed'];
+        $play->unreachable = $stats['unreachable'];
+        $play->skipped = $stats['skipped'];
+        $play->rescued = $stats['rescued'];
+        $play->ignored = $stats['ignored'];
+        $play->save();
     }
 }
