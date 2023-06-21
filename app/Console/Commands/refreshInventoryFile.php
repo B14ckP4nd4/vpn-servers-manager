01060a82fb4b0a309a33926b67a2405c44b006e4
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2023-06-21 13:42:30 +0000

    add ssh proxy env

--- app/Console/Commands/refreshInventoryFile.php
+++ app/Console/Commands/refreshInventoryFile.php
@@ -101,12 +101,16 @@ class refreshInventoryFile extends Command
                 ]);
             }
 
-            $inventoryVariables = array_merge($inventoryVariables,[
-                [
-                    'name' => 'ansible_ssh_common_args',
-                    'val' => '-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ProxyCommand="ssh -W %h:%p -q Tunnel -p 22"',
-                ],
-            ]);
+            # SSH Proxy
+            if(env('ANSIBLE_SSH_PROXY',false)){
+                $inventoryVariables = array_merge($inventoryVariables,[
+                    [
+                        'name' => 'ansible_ssh_common_args',
+                        'val' => '-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ProxyCommand="ssh -W %h:%p -q Tunnel -p 22"',
+                    ],
+                ]);
+            }
+
 
             // generate inventory file Content
             $content = AnsibleFileManager::generateInventoryFile($inventoryHosts , $inventoryVariables);
